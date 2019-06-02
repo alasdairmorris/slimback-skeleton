7b7b49e26eaa842a880b6d3db7de3bad66bf4dbe
Alasdair Morris <amorris@speysoftware.com>
2019-06-02 17:03:33 +0100

    Changed back to simple test page by default, with Bootstrap test page on separate URL

--- app/routes.php
+++ app/routes.php
@@ -6,6 +6,9 @@ $app->get('/', function () use ($app) {
     $app->render('test.html');
 })->name('test');
 
+$app->get('/bootstrap-test', function () use ($app) {
+    $app->render('bootstrap_test.html');
+})->name('bootstrap-test');
 
 // API group
 $app->group('/api', function () use ($app) {
